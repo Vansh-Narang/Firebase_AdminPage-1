07ecf353965932fab1c3939e4c0a35c668c923bc
code-bajju <11212766@mmec.mmdu.org>
2023-03-13 23:59:04 +0530

    Image list added

--- lib/Screens/Signing/DetailPage.dart
+++ lib/Screens/Signing/DetailPage.dart
@@ -1,60 +1,83 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_application_20/Screens/Signing/EventPage.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 
-class DetailPage extends StatelessWidget {
-  final String documentId;
+class EventDetails extends StatelessWidget {
+  final String eventId;
 
-  DetailPage({required this.documentId});
+  EventDetails({required this.eventId});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Session Details'),
+        title: Text('Event Details'),
       ),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('sessions')
-            .doc(documentId)
-            .snapshots(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (!snapshot.hasData) {
+      body: FutureBuilder(
+        future: Future.wait([getEventData(), getEventImage()]),
+        builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(
               child: CircularProgressIndicator(),
             );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error occurred: ${snapshot.error}'),
+            );
+          } else {
+            final eventData = snapshot.data![0] as Map<String, dynamic>;
+            final imageUrl = snapshot.data![1] as String;
+
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  if (imageUrl.isNotEmpty)
+                    Image.network(
+                      imageUrl,
+                      height: 300,
+                    ),
+                  SizedBox(height: 16),
+                  Text(
+                    eventData['title'] ?? '',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Text('Date: ${eventData['date']?.toDate() ?? ''}'),
+                  SizedBox(height: 8),
+                  Text('Start Time: ${eventData['startTime'] ?? ''}'),
+                  SizedBox(height: 8),
+                  Text('End Time: ${eventData['finishTime'] ?? ''}'),
+                  SizedBox(height: 8),
+                  Text('Venue: ${eventData['venue'] ?? ''}'),
+                  SizedBox(height: 8),
+                  Text('Status: ${eventData['status'] ?? ''}'),
+                  SizedBox(height: 8),
+                  Text('Type: ${eventData['type'] ?? ''}'),
+                  SizedBox(height: 8),
+                  Text('Description: ${eventData['description'] ?? ''}'),
+                  SizedBox(height: 16),
+                ],
+              ),
+            );
           }
-
-          Map<String, dynamic> data =
-              snapshot.data!.data() as Map<String, dynamic>;
-
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 16),
-                Text(
-                  data['title'],
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  'Speaker: ${data['speakerName']}',
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 16),
-                Image.network(
-                  data['imageUrl'],
-                  height: 200,
-                  fit: BoxFit.cover,
-                ),
-              ],
-            ),
-          );
         },
       ),
     );
+  }
+
+  Future<Map<String, dynamic>> getEventData() async {
+    final DocumentSnapshot<Map<String, dynamic>> snapshot =
+        await FirebaseFirestore.instance
+            .collection('events')
+            .doc(eventId)
+            .get();
+    return snapshot.data() ?? {};
+  }
+
+  Future<String> getEventImage() async {
+    final snapshot = await FirebaseStorage.instance
+        .ref('eventImages/$eventId.jpg')
+        .getDownloadURL();
+    return snapshot;
   }
 }
