df9587a8f11c2a38c099c884215ffd2e1fd88876
code-bajju <11212766@mmec.mmdu.org>
2023-03-14 23:19:18 +0530

    all pages update

--- lib/Screens/Signing/BottomNavy.dart
+++ lib/Screens/Signing/BottomNavy.dart
@@ -3,8 +3,10 @@ import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter_application_20/Screens/Authenticate/new.dart';
 import 'package:flutter_application_20/Screens/Signing/DetailPage.dart';
 import 'package:flutter_application_20/Screens/Signing/EventPage.dart';
+import 'package:flutter_application_20/Screens/Signing/Logout.dart';
 import 'package:flutter_application_20/Screens/Signing/SignIn.dart';
-import 'EventScheduler.dart';
+import 'package:flutter_application_20/Screens/Signing/EventScheduler.dart';
+import 'EventPage.dart';
 
 class BottomNavy extends StatefulWidget {
   const BottomNavy({Key? key}) : super(key: key);
@@ -16,18 +18,6 @@ class BottomNavy extends StatefulWidget {
 class _BottomNavyState extends State<BottomNavy> {
   int _currentIndex = 0;
   late PageController _pageController;
-
-  List pages = [
-    EventScheduler(),
-    SavedDataPage(),
-    NewPage(),
-    SignIn(),
-  ];
-  int currentState = 0;
-
-  void ontap(int index) {
-    setState(() {});
-  }
 
   @override
   void initState() {
@@ -42,40 +32,25 @@ class _BottomNavyState extends State<BottomNavy> {
   }
 
   @override
-  void _goToExternalClass(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => EventScheduler()),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
+    var pages = [
+      SavedDataPage(),
+      EventScheduler(),
+      SignIn(),
+      LogoutPage(),
+    ];
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Bottom Navigation"),
-        centerTitle: true,
-      ),
+      // appBar: AppBar(
+      //   title: Text("Bottom Navigation"),
+      //   centerTitle: true,
+      // ),
       body: SizedBox.expand(
         child: PageView(
           controller: _pageController,
           onPageChanged: (index) {
             setState(() => _currentIndex = index);
           },
-          children: <Widget>[
-            Container(
-              color: Colors.blueGrey,
-            ),
-            Container(
-              color: Colors.red,
-            ),
-            Container(
-              color: Colors.green,
-            ),
-            Container(
-              color: Colors.blue,
-            ),
-          ],
+          children: pages, // Use the pages list instead of containers
         ),
       ),
       bottomNavigationBar: BottomNavyBar(
@@ -87,16 +62,16 @@ class _BottomNavyState extends State<BottomNavy> {
         },
         items: <BottomNavyBarItem>[
           BottomNavyBarItem(
-              title: Text('Home Page'),
-              icon: Icon(Icons.home),
+              title: Text('Events'),
+              icon: Icon(Icons.pages),
               activeColor: Colors.blueGrey),
           BottomNavyBarItem(
-              title: Text('Events'),
-              icon: Icon(Icons.apps),
+              title: Text('Events Add'),
+              icon: Icon(Icons.post_add),
               activeColor: Colors.blueGrey),
           BottomNavyBarItem(
               title: Text('Members'),
-              icon: Icon(Icons.chat_bubble),
+              icon: Icon(Icons.people),
               activeColor: Colors.blueGrey),
           BottomNavyBarItem(
               title: Text('Log out'),
