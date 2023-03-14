f0f9ae5e03cd6d7b41312935380f0a7df81cf92f
code-bajju <11212766@mmec.mmdu.org>
2023-03-14 23:19:43 +0530

    Logout Implemented

---
+++ lib/Screens/Signing/Logout.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LogoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Logout'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 70.0,
+              backgroundImage: AssetImage('assets/avatar.jpg'),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'User',
+              style: TextStyle(
+                fontSize: 30.0,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            ElevatedButton(
+              child: Text(
+                'Logout',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
